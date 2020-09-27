3583dd2c558b5d46c651c73ff8042dff1ae3380c
Loli <Loli@Lolis-MBP.lan>
2020-09-27 11:53:10 +0800

    调整Logger代码

--- main/cmd.go
+++ main/cmd.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"daisy/config"
-	"daisy/logger"
 	"daisy/redis"
 	"flag"
 	"fmt"
@@ -43,22 +42,22 @@ func main() {
 		var result []string
 		result, cursor, err = source.Scan(cursor, c.Source.Prefix + "*", 1000).Result() // Redis >= 2.8
 		if err != nil {
-			logger.Error("遍历键名错误:", err)
+			fmt.Println("遍历键名错误:", err)
 		}
 		if len(result) != 0 {
-			logger.Info("此次迁移数量:", len(result))
+			fmt.Println("此次迁移数量:", len(result))
 		}
 		for i := 0; i < len(result); i++ {
 			t, err := source.Type(result[i]).Result()
 			if err != nil {
-				logger.Error("获取键名为", result[i], "的类型错误:", err, )
+				fmt.Println("获取键名为", result[i], "的类型错误:", err, )
 				continue
 			}
 			switch t {
 			case redis.TYPE_STRING:
 				expireAt, err := source.TTL(result[i]).Result()
 				if err != nil {
-					logger.Error("获取", result[i], "的过期时间错误", err.Error())
+					fmt.Println("获取", result[i], "的过期时间错误", err.Error())
 					break
 				}
 				if expireAt == -2 {
@@ -66,18 +65,18 @@ func main() {
 				}
 				value, err := source.Get(result[i]).Result()
 				if err != nil {
-					logger.Error("获取STRING键", result[i], "错误:", err.Error())
+					fmt.Println("获取STRING键", result[i], "错误:", err.Error())
 					break
 				}
 				newKeyName := strings.Replace(result[i], c.Source.Prefix, c.Target.Prefix, 1)
 				err = target.Set(newKeyName, value, expireAt).Err()
 				if err != nil {
-					logger.Error("保存", newKeyName, "错误:", err.Error())
+					fmt.Println("保存", newKeyName, "错误:", err.Error())
 				}
 			case redis.TYPE_SET:
 				expireAt, err := source.TTL(result[i]).Result()
 				if err != nil {
-					logger.Error("获取", result[i], "的过期时间错误", err.Error())
+					fmt.Println("获取", result[i], "的过期时间错误", err.Error())
 					break
 				}
 				if expireAt == -2 {
@@ -85,29 +84,29 @@ func main() {
 				}
 				value, err := source.SMembers(result[i]).Result()
 				if err != nil {
-					logger.Error("获取SET键", result[i], "错误:", err.Error())
+					fmt.Println("获取SET键", result[i], "错误:", err.Error())
 					break
 				}
 				newKeyName := strings.Replace(result[i], c.Source.Prefix, c.Target.Prefix,1)
 				err = target.SAdd(newKeyName, value).Err()
 				if err != nil {
-					logger.Error("保存", newKeyName, "失败:", err.Error())
+					fmt.Println("保存", newKeyName, "失败:", err.Error())
 					break
 				}
 				if expireAt >= 0 {
 					ok, err := target.Expire(newKeyName, expireAt).Result()
 					if err != nil {
-						logger.Error("设置", newKeyName, "过期时间错误:", err.Error())
+						fmt.Println("设置", newKeyName, "过期时间错误:", err.Error())
 						break
 					}
 					if !ok {
-						logger.Error("设置", newKeyName, "过期时间失败")
+						fmt.Println("设置", newKeyName, "过期时间失败")
 					}
 				}
 			case redis.TYPE_ZSET:
 				expireAt, err := source.TTL(result[i]).Result()
 				if err != nil {
-					logger.Error("获取", result[i], "的过期时间错误", err.Error())
+					fmt.Println("获取", result[i], "的过期时间错误", err.Error())
 					break
 				}
 				if expireAt == -2  {
@@ -115,29 +114,29 @@ func main() {
 				}
 				value, err := source.ZRangeWithScores(result[i], 0, -1).Result()
 				if err != nil {
-					logger.Error("获取ZSET键", result[i], "失败:", err.Error())
+					fmt.Println("获取ZSET键", result[i], "失败:", err.Error())
 					break
 				}
 				newKeyName := strings.Replace(result[i], c.Source.Prefix, c.Target.Prefix, 1)
 				err = target.ZAdd(newKeyName, value...).Err()
 				if err != nil {
-					logger.Error("保存", newKeyName, "失败:", err.Error())
+					fmt.Println("保存", newKeyName, "失败:", err.Error())
 					break
 				}
 				if expireAt >= 0 {
 					ok, err := target.Expire(newKeyName, expireAt).Result()
 					if err != nil {
-						logger.Error("设置", newKeyName, "过期时间错误:", err.Error())
+						fmt.Println("设置", newKeyName, "过期时间错误:", err.Error())
 						break
 					}
 					if !ok {
-						logger.Error("设置", newKeyName, "过期时间失败")
+						fmt.Println("设置", newKeyName, "过期时间失败")
 					}
 				}
 			case redis.TYPE_HASH:
 				expireAt, err := source.TTL(result[i]).Result()
 				if err != nil {
-					logger.Error("获取", result[i], "的过期时间错误", err.Error())
+					fmt.Println("获取", result[i], "的过期时间错误", err.Error())
 					break
 				}
 				if expireAt == -2 {
@@ -145,7 +144,7 @@ func main() {
 				}
 				value, err := source.HGetAll(result[i]).Result()
 				if err != nil {
-					logger.Error("获取HASH键", result[i], "失败:", err.Error())
+					fmt.Println("获取HASH键", result[i], "失败:", err.Error())
 					break
 				}
 				val := map[string]interface{}{}
@@ -155,23 +154,23 @@ func main() {
 				newKeyName := strings.Replace(result[i], c.Source.Prefix, c.Target.Prefix, 1)
 				err = target.HMSet(newKeyName, val).Err()
 				if err != nil {
-					logger.Error("保存", newKeyName, "失败:", err.Error())
+					fmt.Println("保存", newKeyName, "失败:", err.Error())
 					break
 				}
 				if expireAt >= 0 {
 					ok, err := target.Expire(newKeyName, expireAt).Result()
 					if err != nil {
-						logger.Error("设置", newKeyName, "过期时间错误:", err.Error())
+						fmt.Println("设置", newKeyName, "过期时间错误:", err.Error())
 						break
 					}
 					if !ok {
-						logger.Error("设置", newKeyName, "过期时间失败")
+						fmt.Println("设置", newKeyName, "过期时间失败")
 					}
 				}
 			case redis.TYPE_LIST:
 				expireAt, err := source.TTL(result[i]).Result()
 				if err != nil {
-					logger.Error("获取", result[i], "的过期时间错误", err.Error())
+					fmt.Println("获取", result[i], "的过期时间错误", err.Error())
 					break
 				}
 				if expireAt == -2 {
@@ -179,7 +178,7 @@ func main() {
 				}
 				value, err := source.LRange(result[i], 0, -1).Result()
 				if err != nil {
-					logger.Error("获取LIST键", result[i], "失败:", err.Error())
+					fmt.Println("获取LIST键", result[i], "失败:", err.Error())
 					break
 				}
 				for i, j := 0, len(value)-1; i < j; i, j = i+1, j-1 {
@@ -188,24 +187,24 @@ func main() {
 				newKeyName := strings.Replace(result[i], c.Source.Prefix, c.Target.Prefix, 1)
 				err = target.LPush(newKeyName, value).Err()
 				if err != nil {
-					logger.Error("保存", newKeyName, "失败:", err.Error())
+					fmt.Println("保存", newKeyName, "失败:", err.Error())
 					break
 				}
 				if expireAt >= 0 {
 					ok, err := target.Expire(newKeyName, expireAt).Result()
 					if err != nil {
-						logger.Error("设置", newKeyName, "过期时间错误:", err.Error())
+						fmt.Println("设置", newKeyName, "过期时间错误:", err.Error())
 						break
 					}
 					if !ok {
-						logger.Error("设置", newKeyName, "过期时间失败")
+						fmt.Println("设置", newKeyName, "过期时间失败")
 					}
 				}
 			}
 		}
 
 		if cursor == 0 {
-			logger.Info(fmt.Sprintf("迁移完成, 用时 %v", time.Since(startAt)))
+			fmt.Println(fmt.Sprintf("迁移完成, 用时 %v", time.Since(startAt)))
 			break
 		}
 	}
--- redis/client.go
+++ redis/client.go
@@ -1,20 +1,20 @@
 package redis
 
 import (
-	"daisy/logger"
+	"fmt"
 	"github.com/go-redis/redis"
 )
 
 func New(url string) *redis.Client {
 	opt, err := redis.ParseURL(url)
 	if err != nil {
-		logger.Error("解析链接字符串", url, "失败:", err.Error())
+		fmt.Println("解析链接字符串", url, "失败:", err.Error())
 	}
 	client := redis.NewClient(opt)
 	err = client.Ping().Err()
 	if err != nil {
-		logger.Error("连接", url, "失败:", err.Error())
+		fmt.Println("连接", url, "失败:", err.Error())
 	}
-	logger.Info("连接到:", url)
+	fmt.Println("连接到:", url)
 	return client
 }
